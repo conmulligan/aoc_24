a8815604f501b3d7fd961865257f9c6dac74df6c
Conor Mulligan <conmulligan@gmail.com>
2024-12-01 21:52:04 +0000

    Add day 1b solution

--- Sources/Day1.swift
+++ Sources/Day1.swift
@@ -7,35 +7,56 @@ struct Day1: Day {
     }
 
     func run() throws {
-        let result = try part1()
-        print("1a result: \(result)")
+        let (left, right) = enumerateLists(input: input)
+
+        let part1Result = try part1(left: left, right: right)
+        print("1a result: \(part1Result)")
+
+        let part2Result = try part2(left: left, right: right)
+        print("1b result: \(part2Result)")
     }
 }
 
 extension Day1 {
-    private func part1() throws -> Int {
-        let lines = input.split(separator: "\n")
-
+    private func enumerateLists(input: String) -> (left: [UInt], right: [UInt]) {
         var leftList = [UInt]()
         var rightList = [UInt]()
-        var distances = [Int]()
 
-        for line in lines {
+        for line in input.split(separator: "\n") {
             let columns = line.split(separator: " ")
             leftList.append(UInt(columns[0])!)
             rightList.append(UInt(columns[1])!)
         }
 
         precondition(leftList.count == rightList.count, "Lists should be equal in length.")
-        
+
         leftList.sort()
         rightList.sort()
 
-        for (idx, value) in leftList.enumerated() {
-            let distance = abs(value.distance(to: rightList[idx]))
+        return (leftList, rightList)
+    }
+}
+
+extension Day1 {
+    private func part1(left: [UInt], right: [UInt]) throws -> Int {
+        var distances = [Int]()
+
+        for (idx, value) in left.enumerated() {
+            let distance = abs(value.distance(to: right[idx]))
             distances.append(distance)
         }
 
         return distances.reduce(0, +)
+    }
+
+    private func part2(left: [UInt], right: [UInt]) throws -> Int {
+        var score = 0
+
+        for value in left {
+            let count = right.count { $0 == value}
+            score += (Int(value) * count)
+        }
+
+        return score
     }
 }
