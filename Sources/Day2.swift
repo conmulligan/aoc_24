1ea1ba57c04e4819323d39c7818099fb73ea0da8
Conor Mulligan <conmulligan@gmail.com>
2024-12-02 23:18:25 +0000

    Add day 2b solution

--- Sources/Day2.swift
+++ Sources/Day2.swift
@@ -7,43 +7,52 @@ struct Day2: Day {
     }
 
     func run() throws {
-        let part1Result = try part1(input: input)
+        let lines: [[Int]] = input.split(separator: "\n").map {
+            String($0).split(separator: " ").compactMap { Int($0) }
+        }
+
+        let part1Result = try part1(lines: lines)
         print("2a result: \(part1Result)")
+
+        let part2Result = try part2(lines: lines)
+        print("2b result: \(part2Result)")
     }
 }
 
 extension Day2 {
-    private func isSafe(report: [Int]) -> Bool {
+    private func isSafe(report: [Int], excludingIndex: Int? = nil) -> Bool {
         let tolerance = 1...3
         var isIncremental: Bool?
 
+        var report = report
+        if let excludingIndex {
+            report.remove(at: excludingIndex)
+        }
+
         for (idx, value) in report.enumerated() {
-            guard idx + 1 < report.count else { break }
+            guard idx + 1 < report.count else { continue }
             let next = report[idx + 1]
             let delta = next - value
-            let incremental = delta > 0
-            if !tolerance.contains(abs(delta)) {
+            let incremented = delta > 0
+            if !tolerance.contains(abs(delta)) || (isIncremental != nil && incremented != isIncremental) {
                 return false
             }
-            if isIncremental != nil && incremental != isIncremental {
-                return false
-            }
-            isIncremental = incremental
+            isIncremental = incremented
         }
 
         return true
     }
 
-    private func part1(input: String) throws -> Int {
-        var safeCount = 0
+    private func part1(lines: [[Int]]) throws -> Int {
+        lines.map { isSafe(report: $0) }.count { $0 }
+    }
 
-        for line in input.split(separator: "\n") {
-            let report = line.split(separator: " ").compactMap { Int($0) }
-            if isSafe(report: report) {
-                safeCount += 1
-            }
-        }
-
-        return safeCount
+    private func part2(lines: [[Int]]) throws -> Int {
+        lines.map { line in
+            line.enumerated()
+                .map { isSafe(report: line, excludingIndex: $0.offset) }
+                .filter { $0 }
+                .count
+        }.count { $0 > 0 }
     }
 }
